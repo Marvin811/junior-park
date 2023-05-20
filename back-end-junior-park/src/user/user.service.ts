6cb9e53b5ba7e5f49af45577c4f18c20c7d91c4a
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2023-05-20 13:26:00 +0300

    feat: add model users

--- back-end-junior-park/src/user/user.service.ts
+++ back-end-junior-park/src/user/user.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+constructor(private prisma: PrismaService) {}
+
+async byId(id: number) {
+    const user = await this.prisma.user.findUnique({
+        where: {
+            id
+        },
+        select: {
+            id: true,
+            email: true,
+            name: true,
+            avatarPath: true,
+            password: false,
+            phone: true,
+            favorites: {
+                select: {
+                    id: true,
+                    name: true,
+                    price: true,
+                    images: true,
+                    slug: true
+                }
+            }
+        }
+    })
+}
+
+}
