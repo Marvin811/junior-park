f925138caa22ab88378a45cc91deab1e407f553e
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2023-06-06 22:55:13 +0300

    feat: added auth page without implementation

--- front-end-junior-park/src/pages/_app.tsx
+++ front-end-junior-park/src/pages/_app.tsx
@@ -1,24 +1,32 @@
-import { store, persistor } from '@/store/store'
-import '@/assets/styles/globals.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import type { AppProps } from 'next/app'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-const queryClient = new QueryClient({
-defaultOptions: {
-  queries: {
-    refetchOnWindowFocus: false
-  }
-}
-})
+import "@/assets/styles/globals.css";
+import AuthProvider from "@/providers/auth-provider/AuthProvider";
+import { TypeComponentAuthFields } from "@/providers/auth-provider/auth-page.types";
+import { store, persistor } from "@/store/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (<QueryClientProvider client={queryClient}>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor} />
-      <Component {...pageProps} />
-    </Provider>
-  </QueryClientProvider>
-  
- )
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & TypeComponentAuthFields) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor} />
+        <AuthProvider Component={{ isOnlyUser: Component.isOnlyUser }}>
+          <Component {...pageProps} />
+        </AuthProvider>
+      </Provider>
+    </QueryClientProvider>
+  );
 }
---
+++ front-end-junior-park/src/providers/auth-provider/AuthProvider.tsx
@@ -0,0 +1,38 @@
+import { useAuth } from "@/hooks/useAuth";
+import { TypeComponentAuthFields } from "./auth-page.types";
+import dynamic from "next/dynamic";
+import { FC, PropsWithChildren, useEffect } from "react";
+import { useActions } from "@/hooks/useActions";
+import { useRouter } from "next/router";
+import { getAccessToken, getRefreshToken } from "@/services/auth/auth.helper";
+
+const DynamicCheckRole = dynamic(() => import("./CheckRole"), { ssr: false });
+
+const AuthProvider: FC<PropsWithChildren<TypeComponentAuthFields>> = ({
+  Component: { isOnlyUser },
+  children,
+}) => {
+
+
+  const { user} = useAuth()
+  const {checkAuth, logout} = useActions()
+
+  const {pathname} = useRouter()
+
+  useEffect(() => {
+    const accessToken = getAccessToken()
+    if (accessToken) checkAuth()
+  }, [])
+  useEffect(() => {
+    const refreshToken = getRefreshToken()
+    if (!refreshToken && user) logout()
+  }, [pathname])
+
+  return isOnlyUser ? (
+    <DynamicCheckRole Component={{ isOnlyUser }}>{children}</DynamicCheckRole>
+  ) : (
+    <>{children}</>
+  );
+};
+
+export default AuthProvider;
